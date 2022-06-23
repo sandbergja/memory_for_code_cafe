0ad3f081a59092642d32463443c80e83a9215d5f
Jane Sandberg <js7389@princeton.edu>
2022-06-23 09:59:12 -0700

    update

--- big_file.rb
+++ big_file.rb
@@ -1,6 +1,12 @@
+MARC_NS = 'http://www.loc.gov/MARC21/slim'
+XML_PATH =  'big_file.xml'
+
 require 'nokogiri'
-File.open("big_file.xml") do |file|
-  10.times do
-    Nokogiri::XML(file)
-  end
+require 'byebug'
+modified_xml = (1..100).map do |i|
+  xml = Nokogiri::XML(File.read(XML_PATH))
+  subfield = xml.xpath('//marc:datafield[@tag="245"]/marc:subfield[@code="a"]', 'marc' => MARC_NS).first
+  subfield.content = "#{i} subfield.content"
+  File.write(XML_PATH, xml)
+  xml
 end
